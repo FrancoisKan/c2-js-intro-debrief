9695227ebc0f8f06e797d4a07b69265ff9893cbf
FrancoisKan <kandacer77@gmail.com>
2021-04-26 12:42:22 -0400

    I have made an edit to a file!

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let myNumber = 11
+//Check that the value of the myNumber is of type number
+if (typeof myNumber !== number) {
+    console.log('This is not a number');
+} else if {(myNumber  %3 );
+    console.log('Gen');
+} else if (myNumber %5);
+    console.log('Buzz');
+} else if {myNumber %3 &&5);
+    console.log('GenBuzz');
+} else if {myNumber
+    console.log('myNumber')
+
+ 
+/*If value of myNumber is a multiple of 3 print 'Gen' to the console
+If value of myNumber is a multiple of 5 print 'Buzz' to the console
+If the value of myNumber is a multiple of both 3 and 5 (ex. 15) print GenBuzz to the console
+If the value of myNumber is any other number print the value of myNumber to the console.*/
